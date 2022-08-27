48c25b126f2666e9bb4132fa445b514a1fb45a08
Erfan Amani <68750908+ErfanAmani2000@users.noreply.github.com>
2022-08-28 00:15:39 +0430

    Add files via upload

---
+++ Gamein_Bot_14010605.py
@@ -0,0 +1,21 @@
+import telebot
+
+
+class Player:
+    def __init__(self):
+
+
+    def purchase_raw_material(self):
+
+
+    def produce(self):
+
+
+class Gamein(Player):
+    def __init__(self):
+        super().__init__()
+
+    def sale_raw_material(self):
+
+
+    def purchase_form_player(self):
